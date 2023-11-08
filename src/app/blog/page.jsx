43b5a76734ffc3f75b070d4c1f6d361a734593a4
waleed Khano <khanow99@gmail.com>
2023-11-09 00:10:13 +0500

    blog page are uncomment for testing

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,17 +5,17 @@ import Image from 'next/image'
 
 const getData = async () => {
 
-//   try {
-//     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
-//       cache: "no-store",
-//     });
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch")
-//     }
-//     return res.json();
-//   } catch (error) {
-//     throw error;
-//   }
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch")
+    }
+    return res.json();
+  } catch (error) {
+    throw error;
+  }
 }
 
 
@@ -23,11 +23,11 @@ const getData = async () => {
 
 async function Blog() {
 
-  // const data = await getData();
+  const data = await getData();
 
   return (
     <div className={styles.maincontainer}>
-      {/* {
+      {
         data.map((item) => (
           <Link key={item._id} href={`/blog/${item._id}`} className={styles.container} >
             <div className={styles.imgContainer}>
@@ -46,7 +46,7 @@ async function Blog() {
             </div>
           </Link>
         ))
-      } */}
+      }
 
     </div>
   )
